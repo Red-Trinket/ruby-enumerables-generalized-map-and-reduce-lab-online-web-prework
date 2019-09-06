1293f4efbcc6cc72637269ceb161c377b70732a1
Matthew Jones <matts3371@gmail.com>
2019-09-06 04:32:18 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,5 +1,12 @@
 # Your Code Here
-def my_own_map(ary)
-  yield
-  return ary
+def my_own_map(array)
+  def my_map(array)
+  new_array = []
+
+  for element in array
+    new_array.push yield element
+  end
+
+  new_array
+end
 end
