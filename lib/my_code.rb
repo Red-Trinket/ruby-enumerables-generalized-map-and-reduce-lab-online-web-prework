3690422495a73a2b77ccbca9469e86228534b0ec
Matthew Jones <matts3371@gmail.com>
2019-09-06 04:32:51 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,6 +1,5 @@
 # Your Code Here
-def my_own_map(array)
-  def my_map(array)
+def my_map(array)
   new_array = []
 
   for element in array
@@ -8,5 +7,4 @@ def my_own_map(array)
   end
 
   new_array
-end
 end
